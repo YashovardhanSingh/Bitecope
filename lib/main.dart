303f547e27c25d56f14f404291a96357ea4a408c
Yashovardhan Singh <yashovardhansinghrajput@gmail.com>
2020-11-24 11:22:18 +0530

    Add QR-Code Scanner

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,45 @@
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 void main() => runApp(
   MaterialApp(
-    home: Login(),
+    home: Scan(),
     title: "MonoFYI",
     debugShowCheckedModeBanner: false,
-    theme: ThemeData(
+    theme:  ThemeData(
       primarySwatch: Colors.blue,
     ),
   ),
 );
 
-class Login extends StatefulWidget {
+class Scan extends StatefulWidget {
   @override
-  _LoginState createState() => _LoginState();
+  _ScanState createState() => _ScanState();
 }
 
-class _LoginState extends State<Login> {
+class _ScanState extends State<Scan> {
+  String barcode = "None";
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future scan() async {
+    String result = await FlutterBarcodeScanner.scanBarcode("#ff6666", "Cancel", true, ScanMode.QR);
+    setState(() {
+      this.barcode = result;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "MonoFYI",
-          style: TextStyle(
+          style: const TextStyle(
             fontSize: 35,
             fontWeight: FontWeight.bold,
             fontStyle: FontStyle.italic,
@@ -32,13 +47,28 @@ class _LoginState extends State<Login> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-
-        ],
-      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: RaisedButton(
+                color: Colors.blue,
+                textColor: Colors.white,
+                splashColor: Colors.blueGrey,
+                onPressed: scan,
+                child: const Text("START SCAN"),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 19.0, vertical: 8.0),
+              child: Text("Code: " + barcode, textAlign: TextAlign.center,),
+            )
+          ],
+        ),
+      )
     );
   }
 }
